d677adc0cc41b7e6913084a670533253416bdd36
thibeauf <thibaud.fontaine1@gmail.com>
2018-12-19 09:16:36 +0100

    MAJformulaires

---
+++ connexionV2.php
@@ -0,0 +1,28 @@
+
+
+<?php
+
+    include "bdd.php";
+ 
+
+	$email = $_POST["email"];
+    $mdp = $_POST["mdp"];
+    
+    
+    if(array_key_exists('email', $_POST) && array_key_exists('mdp', $_POST)){
+        $req = $bdd->prepare("SELECT * FROM user WHERE email = ? AND password = ?");
+        $req->execute([$email, $mdp]);
+        $user = $req->fetch();
+    
+        $result = false;
+
+        if($user['email'] == $email && $user['password'] == $mdp){
+            $result = true;
+      
+         }
+         else{
+             $result = false;
+    }
+    echo json_encode(["result" => $result]);
+}    
+?>
