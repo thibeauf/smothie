556b1c46be4fb9eabb74ccf1d04cf7d7e334dc15
psyloute <p1991gv@gmail.com>
2018-12-20 13:49:19 +0100

    git ignore ajouté bd

--- bdd.php
+++ bdd.php
@@ -3,7 +3,7 @@ $bdd = new PDO
     (
         'mysql:host=localhost;dbname=smooth_maker;charset=UTF8',
         'root',
-        'troiswa',
+        '',
         [
             PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC
         ]
