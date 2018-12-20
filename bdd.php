6a5e55143f97930fb47297632d675b72aa31d8c0
Toninho86 <you@example.com>
2018-12-20 09:35:23 +0100

    accueil correct

--- bdd.php
+++ bdd.php
@@ -1,10 +1,11 @@
 <?php
-$bdd = new PDO(
-    'mysql:host=localhost;dbname=smooth_maker;charset=UTF8',
-    'root',
-    'troiswa',
-    [
-        PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC
-    ]
-);
+$bdd = new PDO
+    (
+        'mysql:host=localhost;dbname=smooth_maker;charset=UTF8',
+        'root',
+        'troiswa',
+        [
+            PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC
+        ]
+    );
 ?>
